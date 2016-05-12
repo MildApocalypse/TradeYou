6032879dbfac4d1960ed356b554ca57dcec3752c
JoshMina <jjmoona0@gmail.com>
2016-05-12 18:17:42 +1200

    Added login, amd delete file temporarily on index page. Delete, display and add to database are now working as intended

--- routes/listings.js
+++ routes/listings.js
@@ -5,8 +5,9 @@ var router = express.Router();
 
 router.get('/', function(req,res,next){
 	res.send("listing site is working");
-  var username=req.query.email;
+  var username=req.query.usermail;
   var password=req.query.password;
+  var toDelete=req.query.deleter;
   console.log(username);
   console.log(password);
  // var realname
@@ -18,11 +19,11 @@ router.get('/', function(req,res,next){
   		}
   	console.log('Connected to database');
   	//add a user 
-  	client.query("INSERT INTO Users (Username, Realname, Password) VALUES ('"+req.query+"', 'josh', '"+pass+"');", function(error,result){
+  	client.query("INSERT INTO Users (Username, Realname, Password) VALUES ('"+username+"', 'josh', '"+password+"');", function(error,result){
     //Check the data in table
     //client.query("SELECT * FROM Users;", function(error,result){
     //delete item 
-    //client.query("DELETE FROM Users WHERE username=('test')", function(error,result){
+    //client.query("DELETE FROM Users WHERE username=('"+toDelete+"')", function(error,result){
     done();
     if(error){
       console.error('Failed to execute query');
