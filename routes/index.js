f1752f99642551ae9d5f7502b0274036f6bb5a60
bucklawr <lawrencebuck@windowslive.com>
2016-05-13 14:17:46 +1200

    able to extend .../node_modules/jade-bootstrap/_bootstrap

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', function(res, next){
+  res.render('login_welcome');
+});
+
 module.exports = router;
