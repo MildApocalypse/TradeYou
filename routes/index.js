4408110e96bd86e49ea9656d1e19a4395760e950
bucklawr <lawrencebuck@windowslive.com>
2016-05-20 20:41:50 +1200

    Broken login crap, will fix tommorrow

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TradeYou' });
+    res.render('index', { title: 'TradeYou' });
 });
 
 module.exports = router;
