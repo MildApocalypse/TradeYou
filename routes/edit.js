31e6da8840d60a0ab73e786572ab0e59062a0579
JoshMina <jjmoona0@gmail.com>
2016-05-23 06:20:36 +1200

    Categories are now working for all jade files. Shopping card icon has been put in browse and purchases page. User can now edit items owned. Purchase page working

---
+++ routes/edit.js
@@ -0,0 +1,33 @@
+var pg=require('pg').native;
+var database = "postgres://bahilmyqemykri:iKowEPn0umVbHXossXNsu_abmX@ec2-54-235-119-42.compute-1.amazonaws.com:5432/d6ap3ia4rlhq77";
+var express = require('express');
+var router = express.Router(); 
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+ // var pathToFile =
+ // pg.defaults.ssl = true;
+  pg.connect(database, function (err, client, done) {
+    if (err) {
+      console.error('Could not connect to the database');
+      console.error(err);
+      return;
+    }
+    console.log('Connected to database');
+      //Check the data in table
+      var sid = req.query.filr;
+      //console.log(sid);
+      
+      client.query("DELETE FROM Listing WHERE sid = '"+sid+"'", function(error,result){
+      done();
+      if (error) {
+        console.error('Failed to execute deletion from listing in buy query');
+        console.error(error);
+        return;
+      } 
+      });
+      res.redirect('/createItem');
+    });
+
+});
+module.exports = router;
---
+++ routes/purchases.js
@@ -0,0 +1,45 @@
+var pg=require('pg').native;
+var database = "postgres://bahilmyqemykri:iKowEPn0umVbHXossXNsu_abmX@ec2-54-235-119-42.compute-1.amazonaws.com:5432/d6ap3ia4rlhq77";
+var express = require('express');
+var router = express.Router(); 
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+ // var pathToFile =
+  pg.defaults.ssl = true;
+  var listings = [];
+  pg.connect(database, function (err, client, done) {
+    if (err) {
+      console.error('Could not connect to the database');
+      console.error(err);
+      return;
+    }
+    console.log('Connected to database');
+      //Check the data in table
+      var uid = 1; //temporarily, this should be current user's uid
+      //console.log(sid);
+      client.query("SELECT * FROM Buy WHERE uid = '"+uid+"' ", function(error,result){
+      if (error) {
+        console.error('Failed to execute selecting from listing in buy query');
+        console.error(error);
+        return;
+      }
+      var i = 0;
+      for (i =0; i < result.rows.length; i++){
+      listings.push(result.rows[i]);      
+      }
+
+      // for (i =0; i < result.rows.length; i++){
+      // console.log(listings[i]);
+      // }
+
+        res.render('purchases', {
+
+          Arrays:listings
+          
+        });
+      done();
+    });
+  });
+});
+module.exports = router;
