4088164b23ccc0e36d9c97afe707052563bbbfaf
JoshMina <jjmoona0@gmail.com>
2016-05-24 00:26:50 +1200

    create Item done

--- routes/itemPage.js
+++ routes/itemPage.js
@@ -6,37 +6,35 @@ var database = "postgres://bahilmyqemykri:iKowEPn0umVbHXossXNsu_abmX@ec2-54-235-
 var express = require('express');
 var router = express.Router();
 
-router.get('/*', function(req,res) {
+router.get('/', function(req,res) {
 
-    // pg.defaults.ssl = true; //Uncomment this if you cannot connect to browse
-    // pg.connect(database, function (err, client, done) {
-    //     if (err) {
-    //         console.error('Could not connect to the database');
-    //         console.error(err);
-    //         return;
-    //     }
-    //     console.log('Connected to database');
-    //     //Check the data in table
-    //     client.query("SELECT * FROM Listing;", function(error,result){
-    //         if (error) {
-    //             console.error('Failed to execute query');
-    //             console.error(error);
-    //             return;
-    //         }
-    //         var i = 0;
-    //         for (i =0; i < result.rows.length; i++){
-    //             listings.push(result.rows[i]);
-    //         }
-    //
-    //         // for (i =0; i < result.rows.length; i++){
-    //         // console.log(listings[i]);
-    //         // }
-    //
-    //         res.render('itemPage', {imagePlaced: false})
-    //         });
-    //     });
-    //
-    res.render('itemPage', {imagePlaced: false})
+    pg.defaults.ssl = true; //Uncomment this if you cannot connect to browse
+    pg.connect(database, function (err, client, done) {
+        if (err) {
+            console.error('Could not connect to the database');
+            console.error(err);
+            return;
+        }
+        console.log('Connected to database');
+        console.log(req.query.txtEdit);
+        console.log(req.query.ls);
+        //Check the data in table
+        client.query("SELECT * FROM Listing WHERE ;", function(error,result){
+            if (error) {
+                console.error('Failed to execute query');
+                console.error(error);
+                return;
+            }
+            done();
+            // for (i =0; i < result.rows.length; i++){
+            // console.log(listings[i]);
+            // }
+    
+            res.render('itemPage', {imagePlaced: false})
+            });
+        });
+    
+   // res.render('itemPage', {imagePlaced: false})
 });
 
 module.exports = router;
